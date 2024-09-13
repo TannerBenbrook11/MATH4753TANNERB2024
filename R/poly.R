5f8a3f468ce2755a43f251461c7b4ed703243d6d
Tanner Benbrook <Tanner.M.Benbrook-1@ou.edu>
2024-09-13 18:10:25 -0500

    new

--- R/poly.R
+++ R/poly.R
@@ -2,12 +2,12 @@
 #'
 #' @param degree degree of polynomial
 #' @param coefficients your given coefficients
-#' @param x the variable
+#' @param x the given x value you want to evaluate the polynomial at
 #'
 #' @return a polynomial
 #' @export
 #'
-#' @examples poly(5, [1, 2, 3, 4, 5, 6], x)
+#' @examples \dontrun{poly(5, [1, 2, 3, 4, 5, 6], x)}
 poly <- function(degree, coefficients, x) {
   # degree: the degree of the polynomial
   # coefficients: a vector of coefficients
