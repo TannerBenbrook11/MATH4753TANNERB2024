f5537ba021edee3dcac4d1f8129d111417a22542
Tanner Benbrook <Tanner.M.Benbrook-1@ou.edu>
2024-09-13 17:51:15 -0500

    polygen

---
+++ R/polygen.R
@@ -0,0 +1,18 @@
+#' polynomial generator
+#'
+#' @param degree degree of polynomial
+#' @param coefficients your given coefficients
+#' @param x the variable
+#'
+#' @return a polynomial
+#' @export
+#'
+#' @examples poly(5, (1, 2, 3, 4, 5, 6), x)
+poly <- function(degree, coefficients, x) {
+  # degree: the degree of the polynomial
+  # coefficients: a vector of coefficients
+  # x: the value(s) for which to evaluate the polynomial
+
+  polynomial <- sum(coefficients * (x ^ (0:degree)))
+  return(polynomial)
+}
