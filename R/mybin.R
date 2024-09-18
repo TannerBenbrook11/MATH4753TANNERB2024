ca229e6ec0b5c4c3e70822da5b2c38767685c281
Tanner Benbrook <Tanner.M.Benbrook-1@ou.edu>
2024-09-18 14:30:57 -0500

    adding mybin function

---
+++ R/mybin.R
@@ -0,0 +1,26 @@
+#' Title mybin function
+#'
+#' @param iter the iterator
+#' @param n number of times it is tested
+#' @param p the probability
+#'
+#' @return a table of relative frequencies
+#' @export
+#'
+#' @examples mybin(iter=10000,n=100, p=0.5)
+mybin=function(iter=100, n=10, p=0.5){
+  # Generate a matrix of samples, with rows = n and columns = iter
+  sam.mat = matrix(sample(c(1, 0), n * iter, replace = TRUE, prob = c(p, 1 - p)), nrow = n)
+
+  # Sum the number of successes (1s) for each iteration (column-wise)
+  succ = colSums(sam.mat)
+
+  # Create a table of frequencies for the number of successes
+  succ.tab = table(factor(succ, levels = 0:n))
+
+  # Plot the results
+  barplot(succ.tab / iter, col = rainbow(n + 1), main = "Binomial simulation", xlab = "Number of successes")
+
+  # Return the table of relative frequencies
+  return(succ.tab / iter)
+}
